453da7349a35e7814490097abe9414f306a0c042
Pavel Labath <pavel@labath.sk>
2025-04-10 15:41:30 +0200

    mac fixup

--- lldb/unittests/Host/linux/HostTest.cpp
+++ lldb/unittests/Host/linux/HostTest.cpp
@@ -37,85 +37,6 @@ public:
 };
 } // namespace
 
-TEST_F(HostTest, GetProcessInfo) {
-  llvm::Triple triple = HostInfo::GetTargetTriple();
-
-  ASSERT_TRUE(
-      (triple.getOS() == llvm::Triple::OSType::Linux) ||
-      (triple.hasEnvironment() &&
-       triple.getEnvironment() == llvm::Triple::EnvironmentType::Android));
-
-  ProcessInstanceInfo Info;
-  ASSERT_FALSE(Host::GetProcessInfo(0, Info));
-
-  ASSERT_TRUE(Host::GetProcessInfo(getpid(), Info));
-
-  ASSERT_TRUE(Info.ProcessIDIsValid());
-  EXPECT_EQ(lldb::pid_t(getpid()), Info.GetProcessID());
-
-  ASSERT_TRUE(Info.ParentProcessIDIsValid());
-  EXPECT_EQ(lldb::pid_t(getppid()), Info.GetParentProcessID());
-
-  ASSERT_TRUE(Info.ProcessGroupIDIsValid());
-  EXPECT_EQ(lldb::pid_t(getpgrp()), Info.GetProcessGroupID());
-
-  ASSERT_TRUE(Info.ProcessSessionIDIsValid());
-  EXPECT_EQ(lldb::pid_t(getsid(getpid())), Info.GetProcessSessionID());
-
-  ASSERT_TRUE(Info.EffectiveUserIDIsValid());
-  EXPECT_EQ(geteuid(), Info.GetEffectiveUserID());
-
-  ASSERT_TRUE(Info.EffectiveGroupIDIsValid());
-  EXPECT_EQ(getegid(), Info.GetEffectiveGroupID());
-
-  ASSERT_TRUE(Info.UserIDIsValid());
-  EXPECT_EQ(geteuid(), Info.GetUserID());
-
-  ASSERT_TRUE(Info.GroupIDIsValid());
-  EXPECT_EQ(getegid(), Info.GetGroupID());
-
-  EXPECT_TRUE(Info.GetArchitecture().IsValid());
-  EXPECT_EQ(HostInfo::GetArchitecture(HostInfo::eArchKindDefault),
-            Info.GetArchitecture());
-  // Test timings
-  // In some sense this is a pretty trivial test. What it is trying to
-  // accomplish is just to validate that these values are never decreasing
-  // which would be unambiguously wrong. We can not reliably show them
-  // to be always increasing because the microsecond granularity means that,
-  // with hardware variations the number of loop iterations need to always
-  // be increasing for faster and faster machines.
-  ASSERT_TRUE(Host::GetProcessInfo(getpid(), Info));
-  ProcessInstanceInfo::timespec user_time = Info.GetUserTime();
-  static volatile unsigned u = 0;
-  for (unsigned i = 0; i < 10'000'000; i++) {
-    u += i;
-  }
-  ASSERT_TRUE(u > 0);
-  ASSERT_TRUE(Host::GetProcessInfo(getpid(), Info));
-  ProcessInstanceInfo::timespec next_user_time = Info.GetUserTime();
-  ASSERT_TRUE(user_time.tv_sec <= next_user_time.tv_sec ||
-              user_time.tv_usec <= next_user_time.tv_usec);
-
-  struct rlimit rlim;
-  EXPECT_EQ(getrlimit(RLIMIT_NICE, &rlim), 0);
-  // getpriority can return -1 so we zero errno first
-  errno = 0;
-  int prio = getpriority(PRIO_PROCESS, PRIO_PROCESS);
-  ASSERT_TRUE((prio < 0 && errno == 0) || prio >= 0);
-  ASSERT_EQ(Info.GetPriorityValue(), prio);
-  // If we can't raise our nice level then this test can't be performed.
-  int max_incr = PRIO_MAX - rlim.rlim_cur;
-  if (max_incr < prio) {
-    EXPECT_EQ(setpriority(PRIO_PROCESS, PRIO_PROCESS, prio - 1), 0);
-    ASSERT_TRUE(Host::GetProcessInfo(getpid(), Info));
-    ASSERT_TRUE(Info.GetPriorityValue().has_value());
-    ASSERT_EQ(Info.GetPriorityValue().value(), prio - 1);
-    EXPECT_EQ(setpriority(PRIO_PROCESS, PRIO_PROCESS, prio), 0);
-  }
-  ASSERT_TRUE(Info.IsZombie().has_value());
-  ASSERT_FALSE(Info.IsZombie().value());
-}
-
 static llvm::cl::opt<std::string> expect_path("expect-path");
 static llvm::cl::opt<std::string> chdir_to("chdir-to");
 
@@ -154,9 +75,9 @@ TEST(XXX, YYY2) {
     exit(HasFailure() ? 1 : 0);
   }
 
-  FileSpec temp_dir = HostInfo::GetProcessTempDir();
-  ASSERT_TRUE(temp_dir);
-  FileSpec symlink = temp_dir.CopyByAppendingPathComponent("symlink");
+  llvm::SmallString<0> temp_dir;
+  ASSERT_THAT_ERROR(llvm::errorCodeToError(FileSystem::Instance().GetRealPath(HostInfo::GetProcessTempDir().GetPath(), temp_dir)), llvm::Succeeded());
+  FileSpec symlink = FileSpec(temp_dir).CopyByAppendingPathComponent("symlink");
   ASSERT_THAT_ERROR(FileSystem::Instance()
                         .Symlink(symlink, HostInfo::GetProgramFileSpec())
                         .takeError(),
@@ -165,7 +86,7 @@ TEST(XXX, YYY2) {
   llvm::SmallString<0> cwd;
   llvm::sys::fs::current_path(cwd);
   ASSERT_THAT_ERROR(llvm::errorCodeToError(
-                        llvm::sys::fs::set_current_path(temp_dir.GetPath())),
+                        llvm::sys::fs::set_current_path(temp_dir)),
                     llvm::Succeeded());
   auto reset_cwd =
       llvm::make_scope_exit([&] { llvm::sys::fs::set_current_path(cwd); });
@@ -191,9 +112,9 @@ TEST(XXX, YYY3) {
     exit(HasFailure() ? 1 : 0);
   }
 
-  FileSpec temp_dir = HostInfo::GetProcessTempDir();
-  ASSERT_TRUE(temp_dir);
-  FileSpec symlink = temp_dir.CopyByAppendingPathComponent("symlink");
+  llvm::SmallString<0> temp_dir;
+  ASSERT_THAT_ERROR(llvm::errorCodeToError(FileSystem::Instance().GetRealPath(HostInfo::GetProcessTempDir().GetPath(), temp_dir)), llvm::Succeeded());
+  FileSpec symlink = FileSpec(temp_dir).CopyByAppendingPathComponent("symlink");
   ASSERT_THAT_ERROR(FileSystem::Instance()
                         .Symlink(symlink, HostInfo::GetProgramFileSpec())
                         .takeError(),
@@ -202,7 +123,7 @@ TEST(XXX, YYY3) {
   llvm::SmallString<0> cwd;
   llvm::sys::fs::current_path(cwd);
   ASSERT_THAT_ERROR(llvm::errorCodeToError(
-                        llvm::sys::fs::set_current_path(temp_dir.GetPath())),
+                        llvm::sys::fs::set_current_path(temp_dir)),
                     llvm::Succeeded());
   auto reset_cwd =
       llvm::make_scope_exit([&] { llvm::sys::fs::set_current_path(cwd); });
